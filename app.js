d99107ed30ece956a8706473e286db94a8250312
Aleksander Bekrenev <praise2001@yandex.ru>
2022-10-03 23:29:55 +0700

    fix: prettier

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-let express = require('express')
-let config = require('./config/local.json')
-let port = config.server.port
-let albumsRouter = require('./routes/albums')
-let bodyParser = require('body-parser')
+let express = require("express");
+let config = require("./config/local.json");
+let port = config.server.port;
+let albumsRouter = require("./routes/albums");
+let bodyParser = require("body-parser");
 
-let app = express()
+let app = express();
 
-app.use('/albums', albumsRouter)
+app.use("/albums", albumsRouter);
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}`);
+});
