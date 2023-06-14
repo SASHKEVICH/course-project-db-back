b530339e86b4f5e1990e3106a8748d3b3740587e
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-06-14 19:05:59 +0700

    fix(db/route): using dotenv in database / new test route

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,7 +1,9 @@
 import pgPromise from "pg-promise";
-import { IMain, IDatabase } from 'pg-promise';
+import { IMain, IDatabase } from "pg-promise";
 
-const cn: string = process.env.DATABASE_URL ?? '';
+require("dotenv").config();
+
+const cn: string = process.env.DATABASE_URL ?? "";
 const pgp: IMain = pgPromise();
 const db: IDatabase<any> = pgp(cn);
 
